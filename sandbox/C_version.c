75b98d1f16cbd81a77bb02cf0d16974ae455deec
Nikolay <142244267+epsinenta@users.noreply.github.com>
2024-09-07 12:20:41 +0000

    еще чуть чуть

---
+++ sandbox/C_version.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("Berdnikov\nNikolai\nOlegovih\n");
+    return 0;
+}
---
+++ sandbox/first.asm
@@ -0,0 +1,15 @@
+format ELF
+public _start
+msg db "Berdnikov", 0xA, "Nikolai", 0xA, "Olegovih", 0xA, 0
+
+_start:
+    ;инициализация регистров для вывода информации на экран
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, 28
+    int 0x80
+    ;инициализация регистров для успешного завершения работы программы
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
